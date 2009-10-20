5fd9a7771040ddb696bdf4133f9778323e1bf7d4
coremayo <coremayo@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-20 17:51:22 +0000

    fixing tabs

--- trunk/application/models/sites_model.php
+++ trunk/application/models/sites_model.php
@@ -1,38 +1,38 @@
 <?php
 
 /**
-		* Sites_model class handles all communication between controllers and db.
-		*/
+  * Sites_model class handles all communication between controllers and db.
+  */
 class Sites_model extends Model {
 
-		/**
-				* Default constructor for Sites_model class.
-				*/
-		function Sites_model() {
-				parent::Model();
-				$this->load->database();
-		}
+  /**
+    * Default constructor for Sites_model class.
+    */
+  function Sites_model() {
+    parent::Model();
+    $this->load->database();
+  }
 
-		/**
-				* Adds a new site to the database.
-				*
-				* @param String Name of the site to add
-				* @param int Id of the group that will admin the site
-				* @return boolean TRUE if the operation succeeded, otherwise an error message.
-				*/
-		function addSite($siteName, $siteAdmins) {
-				//INSERT INTO site (siteAdmins, siteName) VALUES (1, 'gt');
-		}
+  /**
+    * Adds a new site to the database.
+    *
+    * @param String Name of the site to add
+    * @param int Id of the group that will admin the site
+    * @return boolean TRUE if the operation succeeded, otherwise an error message.
+    */
+  function addSite($siteName, $siteAdmins) {
+    //INSERT INTO site (siteAdmins, siteName) VALUES (1, 'gt');
+  }
 
-		/**
-				* Gets all relavent information for a site.
-				*
-				* @param int ID of the site
-				* @return Array Information about the site or FALSE if id is invalid
-				*/
-		function getSiteInfo($siteId) {
-				// SELECT site.id, site.siteName, cs4911.group.groupName AS siteAdmins, cs4911.group.id AS siteAdminsId FROM site LEFT JOIN cs4911.group ON cs4911.group.id = site.siteAdmins WHERE id = ?;
-		}
+  /**
+    * Gets all relavent information for a site.
+    *
+    * @param int ID of the site
+    * @return Array Information about the site or FALSE if id is invalid
+    */
+  function getSiteInfo($siteId) {
+    // SELECT site.id, site.siteName, cs4911.group.groupName AS siteAdmins, cs4911.group.id AS siteAdminsId FROM site LEFT JOIN cs4911.group ON cs4911.group.id = site.siteAdmins WHERE id = ?;
+  }
 }
 
 ?>
--- trunk/application/models/users_model.php
+++ trunk/application/models/users_model.php
@@ -1,199 +1,198 @@
 <?php
 
 /**
-		* The Users_model class is responsible for interfacing
-		* between the database and controller classes.
-		*/
+  * The Users_model class is responsible for interfacing
+  * between the database and controller classes.
+  */
 class Users_model extends Model {
 
-		/**
-		  * Default constructor for the Users_model class.
-				*/
-		function Users_model() {
-				// call the Model constructor
-				parent::Model();
-		}
+  /**
+    * Default constructor for the Users_model class.
+    */
+  function Users_model() {
+    // call the Model constructor
+    parent::Model();
+  }
 
-		/**
-		  * Returns an array containing all the users in the database.
-				*
-				* @return Array All the users in the database
-				*/
-		function getAllUsers(){
-				$query=$this->db->get('user');
-				if($query->num_rows()>0){
-						// return result set as an associative array
-						return $query->result();
-				}
-		}
+  /**
+    * Returns an array containing all the users in the database.
+    *
+    * @return Array All the users in the database
+    */
+  function getAllUsers(){
+    $query=$this->db->get('user');
+    if($query->num_rows()>0){
+      // return result set as an associative array
+      return $query->result();
+    }
+  }
 
-		/**
-		  * Returns the number of users in the database.
-				*
-				* @return int number of users
-				*/
-		function getNumUsers(){
-				return $this->db->count_all('user');
-		}
+  /**
+    * Returns the number of users in the database.
+    *
+    * @return int number of users
+    */
+  function getNumUsers(){
+    return $this->db->count_all('user');
+  }
 
-		/**
-				* Performs a select query for the specified user's id and attributes.
-				*
-				* @param int The id of the user
-				* @param Array A list of attributes to retrieve, defaults to '*'
-				* @return Array containing the values of each attribute
-				*/
-		function getUserInfo($userId, $fields = '*') {
-				$this->db->select($fields);
-				$this->db->where('id', $userId);
+  /**
+    * Performs a select query for the specified user's id and attributes.
+    *
+    * @param int The id of the user
+    * @param Array A list of attributes to retrieve, defaults to '*'
+    * @return Array containing the values of each attribute
+    */
+  function getUserInfo($userId, $fields = '*') {
+    $this->db->select($fields);
+    $this->db->where('id', $userId);
 
-				$row = $this->db->get('user')->row();
-		}
+    $row = $this->db->get('user')->row();
+  }
 
-		/**
-				* Adds a new user to the database with the email address and password.
-				* Checks that the supplied email address is a valid  email address and
-				* checks the password is at least 8 characters.
-				*
-				* @param String the email address of the user to add
-				* @param String the user's password which will be hashed
-		  *        before being inserted into the database
-				*
-				* @return boolean TRUE if inserting the user succeded,
-	*         otherwise returns an error message.
-				*/
-		function addUser($userEmail, $userPasswd) {
-				// first, check that the Email is valid
-				$emailRegex = '/^[A-Z0-9][A-Z0-9\._%-]*@([A-Z0-9_-]+\.)+[A-Z]{2,4}$/i';
-				if (!preg_match($emailRegex, $userEmail)) {
-						return ('Invalid Email Address');
-				}
+  /**
+    * Adds a new user to the database with the email address and password.
+    * Checks that the supplied email address is a valid  email address and
+    * checks the password is at least 8 characters.
+    *
+    * @param String the email address of the user to add
+    * @param String the user's password which will be hashed
+    *        before being inserted into the database
+    *
+    * @return boolean TRUE if inserting the user succeded,
+    *         otherwise returns an error message.
+    */
+  function addUser($userEmail, $userPasswd) {
+    // first, check that the Email is valid
+    $emailRegex = '/^[A-Z0-9][A-Z0-9\._%-]*@([A-Z0-9_-]+\.)+[A-Z]{2,4}$/i';
+    if (!preg_match($emailRegex, $userEmail)) {
+      return ('Invalid Email Address');
+    }
 
-				// then check that the password is valid
-				$passwdRegex = '/.{8,}/';
-				if (!preg_match($passwdRegex, $userPasswd)) {
-						return ('Password must be at least 8 characters.');
-				}
+    // then check that the password is valid
+    $passwdRegex = '/.{8,}/';
+    if (!preg_match($passwdRegex, $userPasswd)) {
+      return ('Password must be at least 8 characters.');
+    }
 
-				// check if the user already exists
-				$this->db->where('userEmail', $userEmail);
-				$query = $this->db->get('user');
-				if ($query->num_rows() > 0) {
-						return "User Already Exists";
-				}
+    // check if the user already exists
+    $this->db->where('userEmail', $userEmail);
+    $query = $this->db->get('user');
+    if ($query->num_rows() > 0) {
+      return "User Already Exists";
+    }
 
-				// if all is good, add a new user
-				$data = array(
-						'userEmail' => $userEmail,
-						'userPasswdHash' => sha1($userPasswd),
-						'userRegistrationDate' => date('Y-m-d')
-				);
-				$this->db->insert('user',$data);
+    // if all is good, add a new user
+    $data = array(
+      'userEmail' => $userEmail,
+      'userPasswdHash' => sha1($userPasswd),
+      'userRegistrationDate' => date('Y-m-d')
+    );
+    $this->db->insert('user',$data);
 
-				return TRUE;
-		}              
+    return TRUE;
+  }              
 
-		/**
-				* Returns the email address of the user with the given id.
-				*
-				* @param int Id of the user to query
-				* @return String The user's email address
-				*/
-		function getEmail($userId) {
-				$this->db->where('id', $userId);
-				$this->db->select('userEmail');
-				$row = $this->db->get('user')->row();
-				return $row['userEmail'];
-		}
+  /**
+    * Returns the email address of the user with the given id.
+    *
+    * @param int Id of the user to query
+    * @return String The user's email address
+    */
+  function getEmail($userId) {
+    $this->db->where('id', $userId);
+    $this->db->select('userEmail');
+    $row = $this->db->get('user')->row();
+    return $row['userEmail'];
+  }
 
-		/**
-				* Returns the first name of the user with the given id.
-				*
-				* @param int Id of the user to query
-				* @return String The first name of the user
-				*/
-		function getFirstName($userId) {
-				$this->db->select('userFirstName');
-				$this->db->where('id', $userId);
+  /**
+    * Returns the first name of the user with the given id.
+    *
+    * @param int Id of the user to query
+    * @return String The first name of the user
+    */
+  function getFirstName($userId) {
+    $this->db->select('userFirstName');
+    $this->db->where('id', $userId);
 
-				$row = $this->db->get('user')->row();
-				return $row['userFirstName'];
+    $row = $this->db->get('user')->row();
+    return $row['userFirstName'];
 
-		}
+  }
 
-		/**
-				* Sets the user's first name to the given value.
-				*
-				* @param int Id of the user to modify
-				* @param String value to use as the user's first name
-				*/
-		function setFirstName($userId, $firstName) {
-				$data['userFirstName'] = $firstName;
-				$this->db->where('id', $userId);
-				$this->db->update('user', $data);
-		}
+  /**
+    * Sets the user's first name to the given value.
+    *
+    * @param int Id of the user to modify
+    * @param String value to use as the user's first name
+    */
+  function setFirstName($userId, $firstName) {
+    $data['userFirstName'] = $firstName;
+    $this->db->where('id', $userId);
+    $this->db->update('user', $data);
+  }
 
-		/**
-				* Returns the last name of the user with the given id.
-				*
-				* @param int Id of the user to query
-				* @return String The last name of the user
-				*/
-		function getLastName($userId) {
-				$this->db->select('userLastName');
-				$this->db->where('id', $userId);
+  /**
+    * Returns the last name of the user with the given id.
+    *
+    * @param int Id of the user to query
+    * @return String The last name of the user
+    */
+  function getLastName($userId) {
+    $this->db->select('userLastName');
+    $this->db->where('id', $userId);
 
-				$row = $this->db->get('user')->row();
-				return $row['userLastName'];
+    $row = $this->db->get('user')->row();
+    return $row['userLastName'];
 
-		}
+  }
 
-		/**
-				* Sets the user's last name to the given value.
-				*
-				* @param int Id of the user to modify
-				* @param String value to use as the user's last name
-				*/
-		function setLastName($userId, $lastName) {
-				$data['userLastName'] = $lastName;
-				$this->db->where('id', $userId);
-				$this->db->update('user', $data);
-		}
+  /**
+    * Sets the user's last name to the given value.
+    *
+    * @param int Id of the user to modify
+    * @param String value to use as the user's last name
+    */
+  function setLastName($userId, $lastName) {
+    $data['userLastName'] = $lastName;
+    $this->db->where('id', $userId);
+    $this->db->update('user', $data);
+  }
 
-		/**
-				* Returns the name of the user in the format 'First Last'
-				*
-				* @param int Id of the user to query
-				* @return String The last name of the user
-				*/
-		function getFullName($userId) {
-				return "$this->getFirstName($userId) $this->getLastName($userId)";
-		}
+  /**
+    * Returns the name of the user in the format 'First Last'
+    *
+    * @param int Id of the user to query
+    * @return String The last name of the user
+    */
+  function getFullName($userId) {
+    return "$this->getFirstName($userId) $this->getLastName($userId)";
+  }
 
-		/**
-				* Checks whether the given credentials are correct.
-				*
-				* @param String Email address of the user to authenticate
-				* @param String Password of the user to authenticate
-				* @return boolean TRUE if credentials are correct, otherwise FALSE
-				*/
-		function authenticate($userEmail, $userPassword) {
-				$sql = 'SELECT id FROM user WHERE userEmail = ? AND userPasswdHash = \''.sha1($userPassword).'\'';
-				$query = $this->db->query($sql, array($userEmail));
-				return ($query->num_rows() > 0);
-		}
+  /**
+    * Checks whether the given credentials are correct.
+    *
+    * @param String Email address of the user to authenticate
+    * @param String Password of the user to authenticate
+    * @return boolean TRUE if credentials are correct, otherwise FALSE
+    */
+  function authenticate($userEmail, $userPassword) {
+    $sql = 'SELECT id FROM user WHERE userEmail = ? AND userPasswdHash = \''.sha1($userPassword).'\'';
+    $query = $this->db->query($sql, array($userEmail));
+    return ($query->num_rows() > 0);
+  }
 
-		/**
-				* Gets the id of the user with the specified email address.
-				*
-				* @param String The user's email address
-				* @return int The id of the user
-				*/
-		function getId($userEmail) {
-				$this->db->where('userEmail', $userEmail);
-				$this->db->select('id');
-				$row = $this->db->get('user')->row();
-				return $row->id;
-		}
-
+  /**
+    * Gets the id of the user with the specified email address.
+    *
+    * @param String The user's email address
+    * @return int The id of the user
+    */
+  function getId($userEmail) {
+    $this->db->where('userEmail', $userEmail);
+    $this->db->select('id');
+    $row = $this->db->get('user')->row();
+    return $row->id;
+  }
 }
