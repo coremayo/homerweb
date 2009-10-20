ca12a083a005f5f541f0144712f6ad7582b237b5
coremayo <coremayo@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-20 03:57:31 +0000

    added announcement table to database added creation date to resource table added mysql workbench file moved test data to separate file

---
+++ trunk/createTestData.sql
@@ -0,0 +1,7 @@
+
+--
+-- Dumping data for table `user`
+--
+INSERT INTO `user` (`id`, `userEmail`, `userFirstName`, `userLastName`, `userPasswdHash`, `userRegistrationDate`) VALUES
+(1, 'me@yahoo.com', NULL, NULL, 'cffb0d21c420fdda412eab787bb5fa8e9a62bcd0', '2009-10-19');
+
