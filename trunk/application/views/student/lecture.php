c303bbfe36a4ff68c5ba07b1bb3cfc15cbee396a
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-21 23:25:23 +0000

    These 2 test files did not get committed

---
+++ trunk/application/views/student/lecture.php
@@ -0,0 +1,21 @@
+<div id="main">
+	<div id="level1">
+		<div id="Lecture">
+			<h2>Lecture</h2>
+            <table>
+                <tr>
+                <td id="rightBottom">Date: </td>
+                <td id="bottomborder">Data</td>
+                </tr>
+                <tr>
+                <td id="rightBottom">Topic: </td>
+                <td id="bottomborder">Data</td>
+                </tr>
+                <tr>
+                <td id="rightBottom">Instructor: </td>
+                <td id="bottomborder">Data</td>
+                </tr>
+              </table> 
+		</div>
+	</div>
+</div>
