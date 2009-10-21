74fd72b975f802ef0cc25f08936c81d78a39260c
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-21 02:24:27 +0000

    Test Commit... some rough views

---
+++ trunk/application/views/student/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Chicago Board Review</title>
+		
+		<link rel="stylesheet" href="<?php echo base_url();?>css/student.css" type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" href="<?php echo base_url();?>css/cupertino/jquery-ui-1.7.2.custom.css" type="text/css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="<?php echo base_url();?>css/demo_table.css" type="text/css" media="screen" charset="utf-8">
+		<script type="text/javascript" src="<?php echo base_url();?>js/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>js/jquery-ui-1.7.2.custom.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>js/jquery.validate.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url();?>js/student.js"></script> <!temp>
+        <script type="text/javascript" src="<?php echo base_url();?>js/jquery.dataTables.js"></script> <!temp>
+	</head>
+	<body>
+	
+	<div id="container">
+
+	<div id="header">
+		<h1 id="test">Chicago Review Courses</h1>
+		<small>"The Preferred Neurosurgery Review since 1974"</small>
+		<div id="search">
+			<small><input type="text" name="search" value="Search Keywords..." /> <input type="submit" name="submit" value="Go" /></small>
+		</div>
+	</div>
+
+	<div id="banner">
+		<h1>Lorem Ipsum Dolor</h1>
+		<p>Phasellus interdum orci eu dolor eleifend sit amet fermentum sem ultrices. Aenean accumsan erat sit amet massa fermentum sodales</p>
+	</div>
+	
+	<ul id="navigation">
+		<li><a href="<?php echo base_url();?>student/index">Home</a></li>
+		<li><a href="<?php echo base_url();?>student/courses">Courses</a></li>
+		<li><a href="<?php echo base_url();?>student/qbank">QBank</a></li>
+		<li><a href="<?php echo base_url();?>student/subscriptions">Subscriptions</a></li>
+        <form id="logout" action="<?php echo base_url();?>student/logout" method="post">
+			<input type="submit" value="Logout" id="logout_button">
+			</form>
+	</ul>
+    
+    
---
+++ trunk/application/views/student/subscriptions.php
@@ -0,0 +1,33 @@
+<div id="main">
+	<div id="subs_level1">
+		<div id="subscriptions">
+			<h2>Subscriptions</h2>
+        <br>
+        <table id="user_sub">
+    <thead>
+        <tr>
+            <th>Course</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Days Left</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Course 1</td>
+            <td>st date 1</td>
+            <td>end date 1</td>
+            <td>days left 1</td>
+        </tr>
+        <tr>
+            <td>Course 2</td>
+            <td>st date 2</td>
+            <td>end date 2</td>
+            <td>days left 2</td>
+        </tr>
+        
+    </tbody>
+</table>
+		</div>
+	</div>
+</div>
