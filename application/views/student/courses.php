66238acfc6fb5a55892801d8cf1afb1c4b209b17
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-21 23:25:23 +0000

    These 2 test files did not get committed

---
+++ application/views/student/courses.php
@@ -0,0 +1,10 @@
+<div id="main">
+	<div id="level1">
+		<div id="announcements">
+			<h2>TEST PAGE, FEEL FREE TO DELETE</h2>
+			<p>
+            This is a test! <a href="<?php echo base_url();?>student/lecture">Lecture 1</a> 
+            </p>
+		</div>
+	</div>
+</div>
