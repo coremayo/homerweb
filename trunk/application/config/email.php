b01b0ef6c01223eaee692bd79392178ef5307121
JGFaulk <JGFaulk@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-24 02:56:43 +0000

    Added some icon images for messages in the admin panel

---
+++ trunk/application/config/email.php
@@ -0,0 +1,9 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_port'] = '465';
+$config['smtp_timeout'] = '30';
+$config['smtp_user'] = 'chicagoboardreviewonline@gmail.com';
+$config['smtp_pass'] = 'cbronline';
+$config['newline']= "\r\n"; 
+?>
