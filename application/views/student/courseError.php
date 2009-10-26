01325ee0a8b48e193f8108e51a9c4bacbc3c84a4
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-26 18:32:36 +0000

    Some basic error pages that will be made better at some point

---
+++ application/views/student/courseError.php
@@ -0,0 +1,11 @@
+<div id="main">
+	<div id="subs_level1">
+		<div id="subscriptions">
+			<h2>Course Not Found</h2>
+            <br>
+			You may not have access to this course, or the course may not exist.
+            <br>
+			If you should have access to the course, please contact the site administrator.
+		</div>
+	</div>
+</div>
--- application/views/student/lectures.php
+++ application/views/student/lectures.php
@@ -2,6 +2,7 @@
 	<div id="subs_level1">
 		<div id="subscriptions">
 			<h2><?php echo $classTitle?> Lectures</h2>
+            <br />
                 <table id="user_courses">
                     <thead>
                         <tr>
@@ -17,7 +18,7 @@
                             echo '
                         <tr>       
                             <td><a href="'.base_url().'student/courses/'.$classId.'/'.$info->id.'"style="color: rgb(0,0,0)"><font color="000000"><u>'.$info->lectureTopic.'</u></font></a></td>
-                            <td>'.$info->lectureAdmin.'</td>
+                            <td>'.$this->users_model->getFullName($info->lectureAdmin).'</td>
                             <td>'.$info->lectureStartTime.'</td>
                         </tr>';
                          }
