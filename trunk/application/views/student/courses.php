37f223e3301b75ef79a0b4a08f57f7be7ed24e64
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-26 18:32:36 +0000

    Some basic error pages that will be made better at some point

--- trunk/application/views/student/courses.php
+++ trunk/application/views/student/courses.php
@@ -2,21 +2,22 @@
 	<div id="subs_level1">
 		<div id="subscriptions">
 			<h2>Courses</h2>
-                <table id="user_courses">
-                    <thead>
-                        <tr>
-                            <th>Course</th>
-                            <th>Description</th>
-                            <th>Start Date</th>
-                            <th>End Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $result = $this->subscriptions_model->getValidCourseIDs($this->users_model->getId($this->session->userdata('email')));
-                        foreach ($result ->result() as $res) {
-							$classInfo = $this->classes_model->getClassInfo($res->subscriptionClass);
-   						foreach ($classInfo ->result() as $info) {
-                            echo '
+            <br />
+            <table id="user_courses">
+            	<thead>
+                	<tr>
+                    	<th>Course</th>
+                        <th>Description</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>     
+                     </tr>
+                 </thead>
+                 <tbody>
+                     <?php $result = $this->subscriptions_model->getValidCourseIDs($this->users_model->getId($this->session->userdata('email')));
+                     foreach ($result ->result() as $res) {
+						$classInfo = $this->classes_model->getClassInfo($res->subscriptionClass);
+   					 foreach ($classInfo ->result() as $info) {
+                        echo '
                         <tr>       
                             <td><a href="'.base_url().'student/courses/'.$info->id.'"style="color: rgb(0,0,0)"><font color="000000"><u>'.$info->classTitle.'</u></font></a></td>
                             <td>'.$info->classDesc.'</td>
@@ -25,8 +26,8 @@
                         </tr>';
                         } }
                         ?>
-                    </tbody>
-                </table>
+                   </tbody>
+              </table>
 		</div>
 	</div>
 </div>
