a4cdf52ad42e13ab8ceaa0a3093bf055b24b85bb
ultralisk <ultralisk@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-10-28 03:16:21 +0000

    Updated lecture.php
    Modified createTestData.sql, so make sure to reload the file into the database
    
    Added some more lecture resources test files

--- application/views/student/lecture.php
+++ application/views/student/lecture.php
@@ -6,65 +6,24 @@
                            ?>
                                 Lecture Resources
                         </h2>
-                        <table>
-                        <tr>
-                        <td id="rightBottom">Topic: </td>
-                        <td id="bottomborder"><?php echo $lectureTopic ?></td>
-                        </tr>
-                        <tr>
-                        <td id="rightBottom">Date: </td>
-                        <td id="bottomborder"><?php echo date('D, F d, Y', strtotime($lectureStartTime)); ?>
-                        </td>
-                        </tr>
-                        <tr>
-                        <td id="rightBottom">Time: </td>
-                        <td id="bottomborder"> <?php echo date('h:i:s A', strtotime($lectureStartTime))."  -  ".date('h:i:s A', strtotime($lectureEndTime)) ?>; </td>
-                        </tr>
-                        <tr>
-                        <td id="rightBottom">Instructor: </td>
-                        <td id="bottomborder"><?php echo $this->users_model->getFullName($lectureAdmin) ?></td>
-                        </tr>
-                        <?php 
-                                $data['lectureID'] = $lectureID;
-                                $this->load->view('student/lectureResources', $data);
-                         ?>
-                        <tr></tr>
-                        <tr></tr>
+                        <table id="lecture_info" cellpadding="5">
+                                <tr>
+                                        <th><b> Topic: </b></td>
+                                        <td align="left"><?php echo $lectureTopic ?></td>
+                                        <td><b> Date: </b></td>
+                                        <td align="left"><?php echo date('D, F d, Y', strtotime($lectureStartTime)); ?></td>
+                                </tr>
+                                <tr>
+                                        <td><b> Instructor: </b></td>
+                                        <td align="left"><?php echo $this->users_model->getFullName($lectureAdmin) ?></td>
+                                        <td><b> Time: </b></td>
+                                        <td align="left"><?php echo date('h:i:s A', strtotime($lectureStartTime))."  -  ".date('h:i:s A', strtotime($lectureEndTime)) ?></td>
+                                </tr>
+                                <?php 
+                                        $data['lectureID'] = $lectureID;
+                                        $this->load->view('student/lectureResources', $data);
+                                ?>
                         </table> 
 		 </div>
 	</div>
-<<<<<<< .mine
-=======
-         <div id="subs_level1"> 
-                <div id="lecture_resources">
-                        
-                        <table id="lecture_notes">
-                                <thead>
-                                        <tr>
-                                        <th>Filename</th>
-                                        <th>Description</th>
-                                        <th>Date Posted</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td> Lecture_p1.pdf </d>
-                                    <td> Part one of the lecture notes </d>
-                                    <td> 10/08/2009 </td>
-                                </tr>
-                                <tr>
-                                    <td> Sample_problems.pdf </d>
-                                    <td> Sample problems from with answers </d>
-                                    <td> 10/08/2009 </td>
-                                </tr>
-                                <tr>
-                                    <td> practice_test.pdf </d>
-                                    <td> Some example practice test problems from previous years</d>
-                                    <td> 10/08/2009 </td>
-                                </tr>
-                                </tbody>
-                        </table>
-                 </div>
-       </div> 
->>>>>>> .r68
 </div>
