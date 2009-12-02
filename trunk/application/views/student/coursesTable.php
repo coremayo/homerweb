4c112b98dc631af7ebc156277e8719b50816acf4
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-02 16:43:01 +0000

    Added a file

---
+++ trunk/application/views/student/coursesTable.php
@@ -0,0 +1,6 @@
+<?php echo '<tr>       
+                            <td><a href="'.base_url().'student/courses/'.$courses->id.'"style="color: rgb(0,0,0)"><font color="000000"><u>'.$courses->classTitle.'</u></font></a></td>
+                            <td>'.$courses->classDesc.'</td>
+                            <td>'.$courses->classStartDate.'</td>
+                            <td>'.$courses->classEndDate.'</td>
+                        </tr>'; ?>
