6f0b6a6ee4ef79d3455273db15526b65d8a0b9ff
ultralisk <ultralisk@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-07 21:07:31 +0000

    Removed the Announcements module from the home page. Added an Upcoming courses table.
    I think we should leave the login box as it is. I tried moving it but I don't know css enough to make it look correct.

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,13 @@
 <div id="main">
 <script type="text/javascript"> javascript:changeClass('mainNav'); </script>
 	<div id="level1">
-		<div id="announcements">
-			<h2>Announcements</h2>
+		<div id="about">
+			<h2>About Us</h2>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin purus ligula, adipiscing sed cursus sed, viverra nec nunc. Nunc tempus dignissim suscipit. Aliquam ultrices adipiscing ipsum eget congue. Nulla neque nisl, condimentum quis consequat ac, adipiscing in nisi. Suspendisse potenti.
+				Chicago Review Course develops the most thorough and innovative medical reviews for physicians and practitioners at every stage of their careers. Our rigorous curriculum covers relevant basic sciences and provides in-depth lectures on the leading-edge diagnostic and therapeutic approaches to your particular specialty.
 			</p>
 			<p>
-				Nulla mattis aliquam ipsum. Nam quam lectus, cursus quis rhoncus et, volutpat et tellus.
+			<a href="#">Learn More...</a>
 			</p>
 		</div>
 
@@ -21,19 +21,41 @@
 			<p><small><a href="<?php echo base_url();?>forgotPassword">Forgot Password?</a> | <a href="<?php echo base_url();?>register">Register Here</a></small></p>
 		</div>
 	</div>
-	<div id="level2">
-		<div id="about">
-			<h2>About Us</h2>
-			<p>
-				Chicago Review Course develops the most thorough and innovative medical reviews for physicians and practitioners at every stage of their careers. Our rigorous curriculum covers relevant basic sciences and provides in-depth lectures on the leading-edge diagnostic and therapeutic approaches to your particular specialty.
-			</p>
-			<p>
-			<a href="#">Learn More...</a>
-			</p>
-		</div>
-
-		<div id="image">
-			<img src="<?php echo base_url();?>images/doctor.jpg" alt="Doctor" />
+	
+	<div id="courses_lvl1">
+		<div id="current_courses">
+			<h2>Upcoming Courses</h2>
+			<table id="public_upcoming_courses">
+				<thead>
+					<tr>
+						<th>Course</th>
+						<th>Description</th>
+						<th>Start Date</th>
+						<th>End Date</th>
+						<th>Price</th>
+					 </tr>
+				 </thead>
+				 <tbody>
+					<?php
+						$today = strtotime(date('Y-m-d'));
+						$result = $this->classes_model->getAllClasses();
+						
+						foreach ($result as $classInfo)
+						{
+							if (strtotime(date('Y-m-d', strtotime($classInfo->classStartDate))) > $today)
+							{
+								echo	'<tr>      
+										<td>'.$classInfo->classTitle.'</td>
+										<td>'.$classInfo->classDesc.'</td>
+										<td>'.$classInfo->classStartDate.'</td>
+										<td>'.$classInfo->classEndDate.'</td>
+										<td>'.$classInfo->classPrice.'</td>
+										</tr>';
+							}
+						}
+					?> 
+				</tbody>
+			 </table>
 		</div>
 	</div>
 </div>
