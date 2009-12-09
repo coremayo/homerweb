f5d5e4688daef29ff0bece55e10fb5d8c3972db2
coremayo <coremayo@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-09 18:51:32 +0000

    Making forgot password page look better.

--- trunk/application/views/forgotPassword.php
+++ trunk/application/views/forgotPassword.php
@@ -1,13 +1,16 @@
 <div id="main">
-	<h1>Forgot Password</h2>
-	<p>The forgot password page provides you with a means to reset a forgotten password. Remember to check your email after submitting. </p>
-	
-	<form id="forgotPassword_form" action="<?php echo base_url();?>forgotPassword/confirmEmail" method="post">
-		<h2>Email Confirmation</h2>
+<script type="text/javascript"> javascript:changeClass('mainNav'); </script>
+	<div id="qbank_level1">
+		<div id="qbank">
+      <h2>Forgot Password</h2>
+      <p>The forgot password page provides you with a means to reset a forgotten password. Remember to check your email after submitting. </p>
+      <h2>Email Confirmation</h2>
+      <form id="forgotPassword_form" action="<?php echo base_url();?>forgotPassword/confirmEmail" method="post">
 <?php if ($error) {echo("<div style=\"color:red\">$error</div>");} ?>
-		<p>Email Address: <input type="text" name="email" id="email" class="required email" /></p>
-		<input type="submit" value="Submit" id="forgotPassword_button" />
-	</form>
-	<br>
+      <p>Email Address: <input type="text" name="email" id="email" class="required email" /></p>
+      <input type="submit" value="Submit" id="forgotPassword_button" />
+      </form>
+    </div>
+  </div>
 	<br>
 </div>
