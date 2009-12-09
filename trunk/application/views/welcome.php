e35b92a380c0695087b2720b024953f21cd2a4be
coremayo <coremayo@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-09 18:05:37 +0000

    Issue 4 Adding a registration confirmation page

---
+++ trunk/application/views/welcome.php
@@ -0,0 +1,11 @@
+<div id="main">
+<script type="text/javascript"> javascript:changeClass('mainNav'); </script>
+	<div id="qbank_level1">
+		<div id="qbank">
+			<h2>Welcome</h2>
+        <p>
+        A new user account has just been created for the email address "<?php echo $email; ?>". <a href="../main">Login</a>
+        </p>
+    </div>
+  </div>
+</div>
