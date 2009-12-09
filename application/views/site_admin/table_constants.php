052538ae6384ad5e58e2abbf777ce73ee9220128
JGFaulk <JGFaulk@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-09 07:02:16 +0000

    Started implementing the resources page.

--- application/views/site_admin/table_constants.php
+++ application/views/site_admin/table_constants.php
@@ -2,19 +2,21 @@
 	/*------------------------------\
 	|  Table Options                | 
 	\------------------------------*/
-	if (!defined('USERS'))						define('USERS', 1);
-	if (!defined('COURSES'))					define('COURSES', 2);
-	if (!defined('SCHEDULE'))					define('SCHEDULE', 10);
+	if (!defined('USERS'))							define('USERS', 1);
+	if (!defined('COURSES'))						define('COURSES', 2);
+	if (!defined('SCHEDULE'))						define('SCHEDULE', 10);
 	if (!defined('SUBSCRIPTIONS'))					define('SUBSCRIPTIONS', 11);
-	if (!defined('SHOW_ALL_USERS')) 			define('SHOW_ALL_USERS', 3);
-	if (!defined('SHOW_ALL_COURSES'))			define('SHOW_ALL_COURSES', 4);
-	if (!defined('SHOW_ADMINS_IN_COURSE'))		define('SHOW_ADMINS_IN_COURSE', 5);
-	if (!defined('SHOW_STUDENTS_IN_COURSE'))	define('SHOW_STUDENTS_IN_COURSE', 6);
-	if (!defined('SHOW_ADMINS_NOT_IN_COURSE'))	define('SHOW_ADMINS_NOT_IN_COURSE', 7);
-	if (!defined('SHOW_STUDENTS_NOT_IN_COURSE'))define('SHOW_STUDENTS_NOT_IN_COURSE', 8);
-	if (!defined('SHOW_SCHEDULE_IN_COURSE'))    define('SHOW_SCHEDULE_IN_COURSE', 9);
-	if (!defined('SHOW_ALL_ADMIN_COURSES'))    define('SHOW_ALL_ADMIN_COURSES', 12);
-	if (!defined('SHOW_LECTURE_ADMIN_SCHEDULE'))    define('SHOW_LECTURE_ADMIN_SCHEDULE', 13);
+	if (!defined('RESOURCES'))						define('RESOURCES', 15);
+	if (!defined('SHOW_ALL_USERS')) 				define('SHOW_ALL_USERS', 3);
+	if (!defined('SHOW_ALL_COURSES'))				define('SHOW_ALL_COURSES', 4);
+	if (!defined('SHOW_ADMINS_IN_COURSE'))			define('SHOW_ADMINS_IN_COURSE', 5);
+	if (!defined('SHOW_STUDENTS_IN_COURSE'))		define('SHOW_STUDENTS_IN_COURSE', 6);
+	if (!defined('SHOW_ADMINS_NOT_IN_COURSE'))		define('SHOW_ADMINS_NOT_IN_COURSE', 7);
+	if (!defined('SHOW_STUDENTS_NOT_IN_COURSE'))	define('SHOW_STUDENTS_NOT_IN_COURSE', 8);
+	if (!defined('SHOW_SCHEDULE_IN_COURSE'))    	define('SHOW_SCHEDULE_IN_COURSE', 9);
+	if (!defined('SHOW_ALL_ADMIN_COURSES'))    		define('SHOW_ALL_ADMIN_COURSES', 12);
+	if (!defined('SHOW_LECTURE_ADMIN_SCHEDULE'))	define('SHOW_LECTURE_ADMIN_SCHEDULE', 13);
+	if (!defined('SHOW_ALL_RESOURCES'))				define('SHOW_ALL_RESOURCES', 14);
 	
 	/*------------------------------\
 	|  User Fields                  | 
@@ -52,11 +54,19 @@
 	/*------------------------------\
 	|  Subscription Fields                  | 
 	\------------------------------*/
-	if (!defined('SELECT_FIELD')) 	define('SELECT_FIELD', 1);
-	if (!defined('SUB_FIELD')) 		define('SUB_FIELD', 2);
-	if (!defined('EMAIL_FIELD')) 	define('EMAIL_FIELD', 4);
-	if (!defined('FNAME_FIELD')) 	define('FNAME_FIELD', 8);
-	if (!defined('LNAME_FIELD')) 	define('LNAME_FIELD', 16);
+	if (!defined('SELECT_FIELD')) 		define('SELECT_FIELD', 1);
+	if (!defined('SUB_FIELD')) 			define('SUB_FIELD', 2);
+	if (!defined('EMAIL_FIELD')) 		define('EMAIL_FIELD', 4);
+	if (!defined('FNAME_FIELD')) 		define('FNAME_FIELD', 8);
+	if (!defined('LNAME_FIELD')) 		define('LNAME_FIELD', 16);
 	if (!defined('STARTDATE_FIELD'))	define('STARTDATE_FIELD', 32);
-	if (!defined('ENDDATE_FIELD'))	define('ENDDATE_FIELD', 64);
+	if (!defined('ENDDATE_FIELD'))		define('ENDDATE_FIELD', 64);
 	if (!defined('SUB_ACTIVE_FIELD')) 	define('SUB_ACTIVE_FIELD', 128);
+	
+	/*------------------------------\
+	|  Resource Fields              | 
+	\------------------------------*/
+	if (!defined('SELECT_FIELD')) 		define('SELECT_FIELD', 1);
+	if (!defined('TITLE_FIELD')) 		define('TITLE_FIELD', 2);
+	if (!defined('DESC_FIELD')) 		define('DESC_FIELD', 4);
+	if (!defined('DATE_FIELD')) 		define('DATE_FIELD', 8);
