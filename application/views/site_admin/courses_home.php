9d86fe328edf63f5bb8472bf01362b6d027332a6
malimber <malimber@617cf16e-b9d2-11de-81bc-59952d0011f9>
2009-12-09 03:13:17 +0000

    Getting closer to admin recognition...

--- application/views/site_admin/courses_home.php
+++ application/views/site_admin/courses_home.php
@@ -22,13 +22,21 @@
 	<hr>
 	<br>
 
-	<button type="button" class="addButton" id="addNewCourseButton" onclick="window.location.href='<?php echo base_url();?>site_admin/courses/add_course'">Add New Course</button>
+	<?php if($siteAdmin){ echo '<button type="button" class="addButton" id="addNewCourseButton" onclick="window.location.href=\''.base_url().'site_admin/courses/add_course\'">Add New Course</button>';}?>
 
 	<?php 
+	
+	if($siteAdmin){
 		$data['ID'] = 'coursesTable';
 		$data['TABLE'] = array(SHOW_ALL_COURSES, null);
 		$data['FIELDS'] = TITLE_FIELD | DESC_FIELD | PRICE_FIELD | SUBLENGTH_FIELD | USERS_FIELD | ADMINS_FIELD | STARTDATE_FIELD | ENDDATE_FIELD;
 		$this->load->view('site_admin/table', $data);
+	}else{
+		$data['ID'] = 'coursesTable';
+		$data['TABLE'] = array(SHOW_ALL_ADMIN_COURSES, $this->users_model->getId($this->session->userdata('email')));
+		$data['FIELDS'] = TITLE_FIELD | DESC_FIELD | PRICE_FIELD | SUBLENGTH_FIELD | USERS_FIELD | ADMINS_FIELD | STARTDATE_FIELD | ENDDATE_FIELD;
+		$this->load->view('site_admin/table', $data);
+	}
 	?>
 </div>
 
