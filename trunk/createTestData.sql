133fd0d376011db7604e58a6348de10d9d85993e
dtatum3@gatech.edu <dtatum3@gatech.edu@617cf16e-b9d2-11de-81bc-59952d0011f9>
2010-03-19 01:13:02 +0000

    The "bug" that was preventing us from adding the .flv is resolved..really it wasn't a bug at all--we just need to add another row in the 'lecture_has_resource' table. Doh!

--- trunk/createTestData.sql
+++ trunk/createTestData.sql
@@ -91,6 +91,7 @@ INSERT INTO `lecture_has_resource` (`lecture_id`, `resource_id`) VALUES
 (1, 2),
 (1, 3),
 (1, 4);
+(1, 5);
 
 INSERT INTO `settings` (`id`, `key`, `value`) VALUES
 (1, 'main_banner_image', 'back_disabled.jpg'),
