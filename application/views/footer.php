ec0355be2c137c1880e524815e36d573de655a64
mikhail.lovell <mikhail.lovell@617cf16e-b9d2-11de-81bc-59952d0011f9>
2010-03-19 13:59:25 +0000

    updating the login bar to be smaller; has all the login information on one line. also put a little space between "first, previous, next" buttons on tables (jquery.dataTables.min.js) deleted unnecessary text in forgotPassword.php, register.php, and footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
 </div>
 		<div id="footer">
-			<p>Copyright <a href="<?php echo base_url();?>main">Chicago Review Courses</a> - All Rights Reserved - Design by Team 29 of Georgia Institute of Technology</p>
+			<p>Copyright <a href="<?php echo base_url();?>main">Chicago Review Courses</a> - All Rights Reserved</p>
 		</div> <!--footer-->
 	</div> <!--container-->
 </body>
