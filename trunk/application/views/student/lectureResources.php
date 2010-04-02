7c6b6ed80de13aaacbceb383410db50c52813c7f
mikhail.lovell <mikhail.lovell@617cf16e-b9d2-11de-81bc-59952d0011f9>
2010-04-02 15:40:22 +0000

    embedded the powerpoint in the resources page in the table. link to source is still hardcoded in for the most part.  i had to convert the pptx file to a ppt because the software used only converts ppt, not pptx.

--- trunk/application/views/student/lectureResources.php
+++ trunk/application/views/student/lectureResources.php
@@ -75,10 +75,27 @@ $(document).ready(function(){
                                         pluginspage="http://www.macromedia.com/go/getflashplayer">
                                         </embed>
                                         </object> -->
-                                        <a href="'.base_url().'resources/Neurosurgery Review Course 2009/Introduction/'.$i->resourceTitle.'.html" target="_blank" >'.$i->resourceTitle.'.'.$i->resourceType.'</a>
+                                        <a href="'.base_url().'resources/Neurosurgery Review Course 2009/Introduction/'.$i->resourceTitle.'.html" target="_blank" width="100%" height="">'.$i->resourceTitle.'.'.$i->resourceType.'</a>
                                         </td>';
                                         
-                                }      
+                                } 
+                                
+                                else if ($i->resourceType == "pptx")
+                                {
+                                echo '<td>
+                                <object id="presentation" width="520px" height="330px" 
+                                classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" 
+                                codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" align="middle">
+                                        <param name="allowScriptAccess" value="sameDomain" />
+                                        <param name="movie" value="'.base_url().'resources/Neurosurgery Review Course 2009/Introduction/'.$i->resourceTitle.'.swf" />
+                                        <param name="quality" value="high" />
+                                        <param name="bgcolor" value="#ffffff" />
+                                        <param name="allowFullScreen" value="true" />
+                                        <embed src="'.base_url().'resources/Neurosurgery Review Course 2009/Introduction/'.$i->resourceTitle.'.swf" quality="high" bgcolor="#ffffff" width="520px" height="330px" name="presentation" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" allowFullScreen="true" />
+                                </object>
+                                <a href="'.$i->resourceLocation.'">'.$i->resourceTitle.'.'.$i->resourceType.'</a>
+                                </td>';
+                                }     
                 	        else 
                 	        {
                 	                echo '<td><a href="'.$i->resourceLocation.'">'.$i->resourceTitle.'.'.$i->resourceType.'</a></td>';
